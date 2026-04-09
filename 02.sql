48d03a5ec496941cd7c67f73b60b8c3f5b33db95
GuiPLDiniz <108154040+GuiPLDiniz@users.noreply.github.com>
2026-04-09 14:36:46 -0300

    Create 02.sql

---
+++ 02.sql
@@ -0,0 +1,4 @@
+-- 2 Encontre todos os produtos na tabela produtos que não têm uma descrição associada
+--(suponha que a coluna de descrição possa ser nula).
+
+SELECT * from produtos where descricao is NULL
